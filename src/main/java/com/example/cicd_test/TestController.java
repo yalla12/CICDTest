d6919dd35d9e04087926a367e961ff8bf03c8e71
Sunho Shin <yalla12@naver.com>
2024-04-12 15:11:40 +0900

    push test

--- src/main/java/com/example/cicd_test/TestController.java
+++ src/main/java/com/example/cicd_test/TestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class TestController {
     @GetMapping("hello")
     public String hello(Model model) {
-        model.addAttribute("data", "hello");
+        model.addAttribute("data", "hello_pushTest");
         return "hello";
     }
 }
